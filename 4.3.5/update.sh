a01ef4cc876e761a3ae7131671e772635b20746c
Bert Roos <Bert-R@users.noreply.github.com>
2021-06-04 19:07:39 +0200

    Based on Liquibase 4.3.5
    Using MariaDB driver 2.7.3

---
+++ 4.3.5/update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ERROR_EXIT_CODE=1
+
+echo "Setting up liquibase"
+: ${USER?"USER not set"}
+: ${PASSWORD?"PASSWORD not set"}
+: ${URL?"URL not set"}
+[ -d /changelogs ] || (echo "Folder /changelogs/ does not exist" ; exit $ERROR_EXIT_CODE)
+cd /changelogs
+CHANGE_LOG=`echo changelog.*`
+[ -f "$CHANGE_LOG" ] || (echo "Cannot find a single change log matching /changelogs/changelog.*" ; exit $ERROR_EXIT_CODE)
+
+cat <<CONF > liquibase.properties
+  driver: org.mariadb.jdbc.Driver
+  classpath:/opt/jdbc_drivers/$DRIVER_JAR
+  url: $URL
+  username: $USER
+  password: $PASSWORD
+CONF
+
+echo "Applying changelogs ..."
+MAX_TRIES=${MAX_TRIES:-1}
+COUNT=1
+while [  $COUNT -le $MAX_TRIES ]; do
+   echo  "Attempting to apply changelogs: attempt $COUNT of $MAX_TRIES"
+   liquibase --logLevel=info --changeLogFile=$CHANGE_LOG update
+   if [ $? -eq 0 ];then
+   	  echo "Changelogs successfully applied"
+      exit 0
+   fi
+   echo "Failed to apply changelogs"
+   sleep 1
+   let COUNT=COUNT+1
+done
+echo "Too many failed attempts"
+exit $ERROR_EXIT_CODE
